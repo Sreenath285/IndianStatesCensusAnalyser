f0a8662d6ea9f46d67cf43159de648a9d732969f
Sreenath285 <sreenath.285@gmail.com>
2022-03-24 12:36:17 +0530

    Throws custom exception, if we give file with incorrect path

--- IndianStatesCensusAnalyser/src/test/java/CensusAnalyserTest.java
+++ IndianStatesCensusAnalyser/src/test/java/CensusAnalyserTest.java
@@ -1,3 +1,4 @@
+import jdk.jfr.Description;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.rules.ExpectedException;
@@ -8,6 +9,7 @@ class CensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\sreen\\Desktop\\Pgms\\IndianStatesCensusAnalyser\\src\\test\\resources\\IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "C:\\Users\\sreen\\Desktop\\Pgms\\IndianStatesCensusAnalyser\\src\\main\\resources\\IndiaStateCensusData.csv";
 
+    @Description("Given Indian States Census CSV file, Check to ensure the number of record matches")
     @Test
     void givenIndianCensusCSVFileReturnsCorrectRecords() {
         try {
@@ -15,5 +17,18 @@ class CensusAnalyserTest {
             int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
             Assert.assertEquals(29,numOfRecords);
         } catch (CensusAnalyserException e) { }
+    }
+
+    @Description("Given Indian State Census CSV file with incorrect path should return an custom exception")
+    @Test
+    void givenIndiaCensusData_WithWrongFileType_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
     }
 }
